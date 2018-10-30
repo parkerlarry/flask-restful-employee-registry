81cff5a05f45e9ab6065f990694336d8d7f97098
Larry Parker <larryparker1@me.com>
2018-10-30 10:50:33 +0100

    update ignore

--- models/credentials.py
+++ models/credentials.py
@@ -1,38 +1,68 @@
 import json
 
+"""
+ I wish to  avoid O(N) iteration in a large user base
 
-# I wish to  avoid O(N) iteration in a large user base
-# user_id_mapping[1] should return "larry"
-# username_mapping['larry'] should  return larry
+ user_id_mapping[0] should return "larry". Time complexity -> O(1)
+ 
+ username_mapping['larry'] should  return "larry". Time complexity -> O(1)
+ 
+ 2 different dictionaries because, separation of information
+
+ my approach will improve time complexity at the cost of space complexity
+
+ reference: https://wiki.python.org/moin/TimeComplexity"
+ 
+"""
+
 
 class Credentials:
-    def __init__(self, filename):
-        self.users = []
-        self.username_mapping = {}
-        self.user_id_mapping = {}
-        self.update_credentials(filename, None, 'get')
 
-    def update_credentials(self, filename, new_user, mode):
+    def __init__(self, filename):
+
+        self.users = []
+
+        self.username_mapping = {}
+
+        self.user_id_mapping = {}
+
+        self.update_credentials(filename=filename, user=None, mode='get')
+
+    def update_credentials(self, filename, user, mode):
+
         # TODO implement error handling
+
         # Edge case:  error if file is empty
+
         # Base case: file is empty and contains: []
 
         if mode == 'add':
-            self.users.append(new_user)
+
+            # to write to disk: first write to memory, then dump to disk
+
+            self.users.append(user)
 
             credentials_file = open(filename, 'w')
+
             json.dump(self.users, credentials_file, indent=4)
+
             credentials_file.close()
 
         elif mode == 'get':
 
+            # to write to memory: first read from disk
+
             credentials_file = open(filename, 'r')
+
             self.users = json.load(credentials_file)
+
             credentials_file.close()
 
         elif mode == 'delete':
 
-            self.users = [i for i in self.users if i['user_name'] != new_user['user_name']]
+            # to delete on disk : first delete in memory, then dump to disk
+
+            self.users = [i for i in self.users if i['user_name'] != user['user_name']]
 
             db_file = open(filename, 'w')
 
@@ -42,23 +72,28 @@ class Credentials:
 
         elif mode == 'update':
 
-            self.users = [i for i in self.users if i['user_name'] != new_user['user_name']]
+            # to update on disk : first delete in memory, then dump to disk
 
-            self.update_credentials(filename, new_user, 'add')
+            self.users = [i for i in self.users if i['user_name'] != user['user_name']]
 
+            self.update_credentials(filename, user, 'add')
 
-        # set comprehension for mapping username to user object
+        # create a dict == {'unique_username_string': employee_object} using set comprehension
+
         self.username_mapping = {user['user_name']: user for user in self.users}
 
-        # set comprehension for mapping user_id to user object
+        # create a dict == {unique_id_integer: employee_object} using set comprehension
+
         self.user_id_mapping = {user['user_id']: user for user in self.users}
 
-        print(self.users)
-
     def find_by_username(self, user_name):
+
         # return the first match, else None
+
         return self.username_mapping.get(user_name, None)
 
     def find_by_userid(self, user_id):
+
         # return the first match, else None
+
         return self.user_id_mapping.get(user_id, None)
--- models/employees.py
+++ models/employees.py
@@ -2,48 +2,77 @@ import json
 
 
 class Employees:
+
     def __init__(self, filename):
+
         self.employees = []
-        self.update_employees(filename, None, 'get')
+
+        self.update_employees(filename=filename, employee=None, mode='get')
 
     def get_employees(self):
+
+        # return a list of all employee information
+
         return self.employees
 
     def size(self):
+
+        # returns the number of employees in the registry
+
         return len(self.employees)
 
     def update_employees(self, filename, employee, mode):
+
         # TODO implement error handling
+
         # Edge case:  error if file is empty
+
         # Base case: file is empty and contains: []
 
         if mode == 'add':
+
+            # to write to disk: first write to memory, then dump to disk
+
             self.employees.append(employee)
 
             db_file = open(filename, 'w')
+
             json.dump(self.employees, db_file, indent=4)
+
             db_file.close()
 
         elif mode == 'get':
 
+            # to write to memory: first read from disk
+
             db_file = open(filename, 'r')
+
             self.employees = json.load(db_file)
+
             db_file.close()
 
         elif mode == 'delete':
+
+            # to delete on disk : first delete in memory, then dump to disk
+
             self.employees = [i for i in self.employees if i['user_name'] != employee['user_name']]
 
             db_file = open(filename, 'w')
+
             json.dump(self.employees, db_file, indent=4)
+
             db_file.close()
 
         elif mode == 'update':
+
+            # to update on disk : first delete in memory, then dump to disk
+
             self.employees = [i for i in self.employees if i['user_name'] != employee['user_name']]
+
             self.update_employees(filename, employee, 'add')
 
-
-        # print(self.employees)
-
     def find_by_username(self, user_name):
+
         # return the first match, else None
+
         return next(filter(lambda user: user['user_name'] == user_name, self.employees), None)
